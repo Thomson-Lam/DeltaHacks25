411fb98b86090e996a6835e91c52d5f74c0c7749
Thomson-Lam <thomson.lam327@gmail.com>
2025-01-11 20:51:30 -0500

    feat: added home page

---
+++ src/Home.tsx
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react-swc'
+
+
+/*
+Home: stats page, background: 3d scene 
+
+*/
+
+export default function Home () {
+    return (
+        <div>
+        <h1>Home</h1>
+        <p>Welcome to the Home page</p>
+        </div>
+    )
+}
