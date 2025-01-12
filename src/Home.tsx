4201c852a4deccbff06b4de107452b5203fe7720
Thomson-Lam <thomson.lam327@gmail.com>
2025-01-11 21:00:00 -0500

    added action page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,16 @@
 import react from '@vitejs/plugin-react-swc'
-
+import Stats from './Stats'
 
 /*
-Home: stats page, background: 3d scene 
+Home: stats page glass pane, background: 3d scene 
 
 */
 
+// return stats 
 export default function Home () {
     return (
         <div>
-        <h1>Home</h1>
-        <p>Welcome to the Home page</p>
+            <Stats />
         </div>
-    )
+    );
 }
